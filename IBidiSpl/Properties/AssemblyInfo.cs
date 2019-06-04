d988c2ce93e9706081d1e18f450226851da96c32
Tony Edgecombe <tonyedgecombe@gmail.com>
2019-06-04 14:48:35 +0100

    Add project files.

---
+++ IBidiSpl/Properties/AssemblyInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("IBidiSpl")]
+[assembly: AssemblyDescription("A wrapper for the IBidiSpl and IBidiSpl2 interfaces")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Tony Edgecombe")]
+[assembly: AssemblyProduct("IBidiSpl")]
+[assembly: AssemblyCopyright("Copyright © Tony Edgecombe 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3022dc5b-7c3a-4ea9-98e6-1ded398b1a66")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1.*")]
