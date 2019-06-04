8ad5f0958c8826afd9bf0eeb52de7b3e9ab1fc5d
Tony Edgecombe <tonyedgecombe@gmail.com>
2019-06-04 15:04:42 +0100

    Added test console program

---
+++ Test/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using IBidiSpl;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bidiRequest = Activator.CreateInstance<BidiRequest>();
+        }
+    }
+}
