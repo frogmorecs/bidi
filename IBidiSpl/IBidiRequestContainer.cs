81f1ae4e57e96be42fd4bc04eaf61ab1b53b896d
Tony Edgecombe <tonyedgecombe@gmail.com>
2019-06-05 08:58:50 +0100

    Added IBidiRequestContainer COM interface

---
+++ IBidiSpl/IBidiRequestContainer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace IBidiSpl
+{
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    [ComImport, Guid("D752F6C0-94A8-4275-A77D-8F1D1A1121AE")]
+    interface IBidiRequestContainer
+    {
+        // virtual HRESULT STDMETHODCALLTYPE AddRequest(
+        // /* [in] */ __RPC__in_opt IBidiRequest *pRequest) = 0;
+
+        Int32 AddRequest([In, MarshalAs(UnmanagedType.Interface)] IBidiRequest pRequest);
+
+        // virtual HRESULT STDMETHODCALLTYPE GetEnumObject(
+        // /* [out] */ __RPC__deref_out_opt IEnumUnknown **ppenum) = 0;
+
+        Int32 GetEnumObject([Out, MarshalAs(UnmanagedType.Interface)] IEnumUnknown ppenum);
+
+        // virtual HRESULT STDMETHODCALLTYPE GetRequestCount(
+        // /* [out] */ __RPC__out ULONG *puCount) = 0;
+
+        Int32 GetRequestCount([Out] UInt32 puCount);
+    }
+}
---
+++ IBidiSpl/IEnumUnknown.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.InteropServices;
+
+namespace IBidiSpl
+{
+    [ComImport, Guid("00000100-0000-0000-C000-000000000046")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    public interface IEnumUnknown
+    {
+        int Next(
+            [In] int celt,
+            [Out, MarshalAs(UnmanagedType.IUnknown)] object rgelt,
+            [Out] int pceltFetched
+        );
+
+        int Skip([In, MarshalAs(UnmanagedType.U4)] int celt);
+        void Reset();
+        void Clone([Out] IEnumUnknown ppenum);
+    }
+}
